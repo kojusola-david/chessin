7747b8019eac7abd8d5fc1994f9a04e0022b2959
LostVoid7 <kojusoladavid7@gmail.com>
2026-01-12 18:04:15 +0100

    chore: initial server setup with fastify, socket.io, and typebox

---
+++ backend/server.ts
@@ -0,0 +1,62 @@
+import Fastify from "fastify";
+import { Server } from "socket.io";
+import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
+import { Type, Static } from "@sinclair/typebox";
+
+// --- Schemas ---
+const MessageSchema = Type.Object({
+  sender: Type.String(),
+  message: Type.String({ minLength: 1, maxLength: 500 }),
+  timestamp: Type.Optional(Type.Number())
+});
+
+// --- Infer Typescript type from schemas ---
+type Message = Static<typeof MessageSchema>;
+
+
+const fastify = Fastify({
+    logger: true
+}).withTypeProvider<TypeBoxTypeProvider>();
+
+fastify.get('/', {
+    schema: {
+        response: {
+            200: Type.Object({ status: Type.String()})
+        }
+    }
+}, async () => {
+    return {status: "ok"}
+})
+
+
+const start = async () => {
+    try {
+        await fastify.ready()
+
+        const io = new Server(fastify.server, {
+            cors: {
+                origin: '*',
+                methods: [ 'GET', 'POST' ]
+            }
+        })
+
+        io.on('connection', (socket) => {
+            console.log(`Socket connected: ${socket.id}`)
+
+            socket.on('message', message => {
+                console.log(`${message.sender} said ${message.message}`);
+                
+            })
+        })
+
+        
+
+        await fastify.listen({port: 3000})
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    
+    }
+}
+
+start();
