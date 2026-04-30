3599a14910451edc36ed0c56967906e6e0778bbe
kojusola-david <kojusoladavid7@gmail.com>
2026-04-30 01:19:48 +0100

    fix: Lobby refactor, fixes #4

---
+++ backend/stores/LobbyStore.ts
@@ -0,0 +1,25 @@
+import { GameRequest } from '@chessin/shared';
+
+class LobbyStore{
+    private lobby: Map<string, GameRequest> = new Map();
+
+    public getRequest(roomId: string): GameRequest | undefined {
+        return this.lobby.get(roomId);
+    }
+
+    public createRequest(roomId: string, request: GameRequest): void {
+        this.lobby.set(roomId, request);
+    }
+    
+    public deleteRequest(roomId: string) {
+        this.lobby.delete(roomId);
+    }
+
+    public getLobby(): GameRequest[] {
+        return Array.from(this.lobby.values());
+    }
+
+    public has(roomId: string): boolean {
+        return this.lobby.has(roomId);
+    }
+}
